57c11dcca647afde13c9f80edcb8dcfde494e742
Daksh Choudhary <ch.daksh01@gmail.com>
2025-04-13 23:56:15 +0530

    Background made

---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <>
+        <div className='absolute top-[3%] w-full py-10 flex items-center justify-center text-zinc-600 text-lg'>Documents</div>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] leading-none tracking-tighter text-zinc-900 text-[14vw]'>Docs.</h1>
+    </>
+  )
+}
+
+export default Background
